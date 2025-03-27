2ed025f79345ca6e7a342e2486b7c0cb3c4441f0
Tur Jean-Baptiste <jean-baptiste.tur@student.junia.com>
2025-03-27 15:03:25 +0100

    Partie 5 : Créer page détail d’un personnage

--- personnage.php
+++ personnage.php
@@ -6,28 +6,29 @@ $dbname = "tur_manga"; // Nom de la base de données
 
 try {
     $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Configuration de PDO pour générer des exceptions en cas d'erreur
+  
     $dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connexion réussie à la base de données"; 
-    
-echo "<h1>Top 5 manga :</h1>";
 
-$resultat = "SELECT * FROM manga ORDER BY 'Année de publication' ASC";
+$resultat = "SELECT * FROM personnage";
 $manga = $dbPDO->prepare($resultat);
 $manga->execute();
 
 $manga = $manga->fetchAll(PDO::FETCH_ASSOC);
 echo "<ul>";
 $i=1;
+echo "<h1>description des personnages <br></h1>";
 foreach($manga as $mangas) {
-   
-   echo "<li>" ."Manga n°$i : ". '<a href="../Partiel Web/personnage.php" target="_blank">' .$mangas['Titre']. "</a>". " sorti en ". $mangas['Année de publication']. " avec une note de ". $mangas['note']. " sur 10" ."<br><br></li>";
-   $i++;
+  echo "Description des personnages <br>" . $mangas["description"] . "<br><br>";
 }
 echo "</ul>";
+echo '<a href="../Partiel Web/index.php>'. "Retour vers l'accueil" . "</a>";
+
 } catch(PDOException $e) {
     echo "La connexion a échouée : " . $e->getMessage();
 }
 
 
 ?>
+
+
